63868d3fe2866679d4f8598ac429df61f0b1501c
SeptiaRamadhaniPutriImiyanti <septiaimiyanti26@gmail.com>
2026-09-12 19:24:23 +0700

    feat: tambah informasi lokasi UMKM

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const promoButton = document.getElementById("promoButton");
+
+promoButton.addEventListener("click", function () {
+  alert("Promo hari ini: Diskon Rp5.000 untuk pembelian Cold Brew Aren!");
+});
