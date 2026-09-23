97c174e58f4bf63e0585523749f32f2d06bf62b3
SeptiaRamadhaniPutriImiyanti <septiaimiyanti26@gmail.com>
2026-09-23 08:22:45 +0700

    feat: tambah medai dan tabel katalog

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("Website Kopi Nusa berhasil dimuat.");
+
+const promoButton = document.getElementById("promoButton");
+
+promoButton.addEventListener("click", function () {
+  alert("Promo hari ini: Diskon Rp5.000 untuk pembelian Cold Brew Aren!");
+});
